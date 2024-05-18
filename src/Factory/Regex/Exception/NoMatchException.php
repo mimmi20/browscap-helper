c3bd386ddb9217868094540656608652dba9a14c
Thomas Müller <mimmi20@live.de>
2024-05-18 14:59:02 +0200

    add new dependencies

--- src/Factory/Regex/Exception/NoMatchException.php
+++ src/Factory/Regex/Exception/NoMatchException.php
@@ -10,11 +10,11 @@
 
 declare(strict_types = 1);
 
-namespace BrowscapHelper\Factory\Regex;
+namespace BrowscapHelper\Factory\Regex\Exception;
 
 use OutOfRangeException;
 
-final class GeneralTvException extends OutOfRangeException
+final class NoMatchException extends OutOfRangeException
 {
     // nothing to do here
 }
