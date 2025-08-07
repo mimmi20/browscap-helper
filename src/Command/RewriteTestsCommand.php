5cebf7a1f3108a8b917cdcb0915c6a1c1a379869
Thomas Müller <mimmi20@live.de>
2025-08-07 06:40:52 +0200

    update detection

--- src/Command/RewriteTestsCommand.php
+++ src/Command/RewriteTestsCommand.php
@@ -1326,8 +1326,6 @@ final class RewriteTestsCommand extends Command
                     || str_contains($v, 'wget http://')
                     || str_contains($v, '<?php')
                     || str_contains($v, '((')
-                    || str_contains($v, '))')
-                    || str_contains($v, '()')
                     || str_contains($v, '<\'')
                     || str_contains($v, '">')
                     || str_contains($v, '/**/');
